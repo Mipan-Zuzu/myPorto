aaa5a05a1d9ed6a5d43f8a871ff54d04089285a9
Mipan-Zuzu <kadekisaka00@gmail.com>
2026-07-08 11:39:23 +0800

    feat: fix aboutComp Unused defineProps

---
+++ src/components/AboutComp.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="flex gap-2 items-start  justify-start flex-col w-72 text-3xl font-bold gradentText mt-20 md:text-5xl md:w-96">
+        <p class="text-xl font-bold text-stone-500">About Me</p>
+        <h1 class="text-5xl">Im A Full Stack Web <span class="text-stone-500">Developer</span></h1>
+        <div class="block">
+        <p class="w-72 text-xl font-medium text-stone-500 md:w-full md:text-xl">Passionate about designing <mark>scalable systems</mark> and building <mark>modern web applications</mark> from concept to deployment. Dedicated to writing <mark>clean, maintainable code</mark> and creating <mark>high-performance solutions</mark>.</p>
+        </div>
+    </div>
+</template>
--- src/layouts/AboutLayout.vue
+++ src/layouts/AboutLayout.vue
@@ -1,16 +1,8 @@
 <script setup lang="ts">
-import type {CardAbout} from "../types/Interface"
-
-const data: CardAbout = {
-    Title: "About Me",
-    Desk : "Experienced in end-to-end development using modern  tech stack, with remote collaboration on international projects. Focused on clean code, performance, and continuous learning"
-}
+import AboutComp from '../components/AboutComp.vue'
 
 </script>
 
 <template>
-    <AboutComp 
-        :Title="data.Title"
-        :Desk="data.Desk"
-    />
+    <AboutComp />
 </template>
